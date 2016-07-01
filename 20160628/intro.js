db12081493fa2978ba24121135f450dcab4941ac
Sam McHaney <luomopuma@gmail.com>
2016-06-30 17:39:27 -0700

    Initial commit
    Initial commit for WEB150

---
+++ 20160628/intro.js
@@ -0,0 +1,6 @@
+ var user = prompt("What is your name?", "Name here.").toUpperCase();
+            var date = new Date();
+            // document.getElementByID("output").innerHTML = "TEST";
+            document.getElementById("output").innerHTML =  "Welcome, " + user + "!<br>" + "The current date is " + date;
+
+
