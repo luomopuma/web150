c4705f8dafab55e61a9286c0f5fbb8a2c6680071
Sam McHaney <luomopuma@gmail.com>
2016-07-06 11:11:07 -0700

    Corrects logic error for Assignment 1

--- Assignment1/vars-ops.js
+++ Assignment1/vars-ops.js
@@ -2,9 +2,8 @@ var taxPercentage = 0.1;
 
 var mealPrice = parseFloat(prompt("Enter the amount of your meal."),"15");
 
-var subTotal = mealPrice + mealPrice * taxPercentage;
 var taxAmt = mealPrice * taxPercentage;
-var total = subTotal + taxAmt;
+var total = mealPrice + taxAmt;
 
 var msg = "<br>Meal Price: $"+mealPrice.toFixed(2);
 msg += "<br>Tax Amount: $"+taxAmt.toFixed(2);
