53ac2b6b83f1349036a81ff3b6c5c563e305bfb0
Sam McHaney <luomopuma@gmail.com>
2016-08-11 11:19:06 -0700

    Adds Assignment 5

---
+++ Assignment5/index.js
@@ -0,0 +1,11 @@
+var $= function(id){ return document.getElementById(id);}
+
+var joinList = function () {
+     
+}
+
+window.onload = function () {
+    $("join_list").onclick = joinList;
+    $("subtotal").focus();  
+}
+
